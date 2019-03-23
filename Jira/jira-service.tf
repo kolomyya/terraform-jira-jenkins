e75ec62efad28dc40e79642a0beddb0d609b6c59
kolomyya <andriyzn4@gmail.com>
2019-03-23 17:06:49 +0000

    changes into service

--- Jira/jira-service.tf
+++ Jira/jira-service.tf
@@ -8,30 +8,13 @@ resource "kubernetes_service" "jira-svs" {
       app = "${kubernetes_pod.jira-pod.metadata.0.labels.app}"
     }
 
-    session_affinity = "ClientIP"
 
     port {
-      port        = 8080
-      target_port = 8080
+      port        = 80
+      target_port = 2000
     }
 
     type = "LoadBalancer"
   }
 }
 
-resource "kubernetes_pod" "jira-pod" {
-  metadata {
-    name = "terraform-jira"
-
-    labels {
-      app = "jira"
-    }
-  }
-
-  spec {
-    container {
-      image = "gcr.io/hightowerlabs/jira:7.3.6-standalone"
-      name  = "jira"
-    }
-  }
-}
