cce1e6ecc5a54d49e6a018917223053e23ab86a8
kolomyya <andriyzn4@gmail.com>
2019-03-25 20:38:25 -0500

    changed port

--- Jira/jira-service.tf
+++ Jira/jira-service.tf
@@ -6,33 +6,15 @@ resource "kubernetes_service" "jira-service" {
 
   spec {
     selector {
-      app = "${kubernetes_pod.jira-pod.metadata.0.labels.app}"
+      app = "jira-pod"
     }
 
 
     port {
       port        = 80
-      target_port = 8080
+      target_port = 2000
     }
 
     type = "LoadBalancer"
   }
 }
-
-resource "kubernetes_pod" "jira-pod" {
-  metadata {
-    name = "jira-test"
-
-    labels {
-      app = "jira"
-    }
-  }
-
-  spec {
-    container {
-      image = "gcr.io/hightowerlabs/jira:7.3.6-standalone"
-      name  = "jira-test"
-    }
-  }
-}
-
